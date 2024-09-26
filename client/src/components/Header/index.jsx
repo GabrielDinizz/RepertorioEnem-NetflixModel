8ef7cdb2dcee187aa589f222f00f5265f021a29e
Gabriel Diniz <gabrieldinizcarreiro@gmail.com>
2024-09-26 16:45:34 -0300

    feat: add page login and register

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -3,19 +3,29 @@ import repertorioLogo from '../../assets/repertorioLogo.png';
 
 function Header() {
     return (
-       <>
+        <>
             <header>
                 <div className={styles.container}>
-                    <div>
-                        <img src={repertorioLogo} alt="" />
+                    <div className={styles.pagesMenu}>
+                        <div>
+                            <img src={repertorioLogo} alt="" />
+                        </div>
+                        <div className={styles.menu}>
+                            <a href="/"><p>Home</p></a>
+                            <a href="/pesquisa"><p>Pesquisar</p></a>
+                        </div>
                     </div>
-                    <div className={styles.menu}>
-                        <a href="/"><p>Home</p></a>
-                        <a href="/pesquisa"><p>Pesquisar</p></a>
+                    <div className={styles.authMenu}>
+                        <div className={styles.login}>
+                            <a href="/login">Login</a>
+                        </div>
+                        <div className={styles.register}>
+                            <a href="/register">Cadastrar-se</a>
+                        </div>
                     </div>
                 </div>
             </header>
-        </> 
+        </>
     )
 }
 
---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,17 @@
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import styles from '../Pesquisa/login.module.css';
+
+import React, { useState, useEffect } from 'react';
+
+function Login() {
+    return (
+        <>
+            <Header />
+                aaa
+            <Footer />
+        </>
+    );
+}
+
+export default Login;
